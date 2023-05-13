fc8b325b2f2bc083b735a31920b54b254a04ffba
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2023-05-13 18:04:11 -0500

    testing shared client state

--- src/app/product/[id]/variation/[vid]/page.tsx
+++ src/app/product/[id]/variation/[vid]/page.tsx
@@ -1,4 +1,11 @@
+import { Counter } from "./Counter";
+
 export default function Variation({ params }: any) {
   console.log("*** Variation params", params);
-  return <div>Variation {params.vid}</div>;
+  return (
+    <div>
+      Variation {params.vid}
+      <Counter />
+    </div>
+  );
 }
